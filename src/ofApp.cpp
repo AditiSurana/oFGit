3557b65ab7bf295f0f51efc65218fab9f73161da
pi-yo <kyamazaki13@gmail.com>
2016-03-02 10:20:00 +0900

    kiyochanges

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -4,12 +4,12 @@
 void ofApp::setup(){
     
     ofBackground(200);
-
+    
 }
 
 //--------------------------------------------------------------
 void ofApp::update(){
-
+    
 }
 
 void ofApp::square(){
@@ -18,63 +18,71 @@ void ofApp::square(){
     ofDrawRectangle(ofGetWidth()/2, ofGetHeight()/2, 100, 100);
 }
 
+
+void ofApp::triangle(){
+    ofSetColor(34, 34, 123);
+    ofFill();
+    ofDrawRectangle(ofGetWidth()/2, ofGetHeight()/2, 100, 100);
+}
+
 //--------------------------------------------------------------
 void ofApp::draw(){
     square();
-
+    triangle();
+    
 }
 
 //--------------------------------------------------------------
 void ofApp::keyPressed(int key){
-
+    
 }
 
 //--------------------------------------------------------------
 void ofApp::keyReleased(int key){
-
+    
 }
 
 //--------------------------------------------------------------
 void ofApp::mouseMoved(int x, int y ){
-
+    
 }
 
 //--------------------------------------------------------------
 void ofApp::mouseDragged(int x, int y, int button){
-
+    
 }
 
 //--------------------------------------------------------------
 void ofApp::mousePressed(int x, int y, int button){
-
+    
 }
 
 //--------------------------------------------------------------
 void ofApp::mouseReleased(int x, int y, int button){
-
+    
 }
 
 //--------------------------------------------------------------
 void ofApp::mouseEntered(int x, int y){
-
+    
 }
 
 //--------------------------------------------------------------
 void ofApp::mouseExited(int x, int y){
-
+    
 }
 
 //--------------------------------------------------------------
 void ofApp::windowResized(int w, int h){
-
+    
 }
 
 //--------------------------------------------------------------
 void ofApp::gotMessage(ofMessage msg){
-
+    
 }
 
 //--------------------------------------------------------------
 void ofApp::dragEvent(ofDragInfo dragInfo){ 
-
+    
 }
